ba5091b36872f31eff84be4ba62ce2151a6d38fa
adwityac <adwityachakraborty@hotmail.com>
2025-11-24 19:34:54 +0530

    Add public images

--- client/src/data/mockItems.js
+++ client/src/data/mockItems.js
@@ -3,44 +3,45 @@ const items = [
     id: "1",
     title: "Lawn Mower",
     description: "Well maintained, available for borrowing.",
-    imageUrl: "/lawnmower.png",
+    imageUrl: "lawnmower.png",
     availability: "available",
   },
   {
     id: "2",
     title: "Baby Stroller",
     description: "Well maintained, available for borrowing.",
-    imageUrl: "/stroller.png",
+    imageUrl: "stroller.png",
     availability: "requested",
   },
   {
     id: "3",
     title: "Drill Kit",
     description: "Well maintained, available for borrowing.",
-    imageUrl: "/drillkit.png",
+    imageUrl: "drillkit.png",
     availability: "available",
   },
   {
     id: "4",
     title: "Camping Tent",
     description: "Well maintained, available for borrowing.",
-    imageUrl: "/camping.png",
+    imageUrl: "camping.png",
     availability: "requested",
   },
   {
     id: "5",
     title: "Keyboard",
     description: "Well maintained, available for borrowing.",
-    imageUrl: "/keyboard.png",
+    imageUrl: "keyboard.png",
     availability: "available",
   },
   {
     id: "6",
     title: "Projector",
     description: "Well maintained, available for borrowing.",
-    imageUrl: "/projector.png",
+    imageUrl: "projector.png",
     availability: "available",
   },
 ]
 
 export default items
+
