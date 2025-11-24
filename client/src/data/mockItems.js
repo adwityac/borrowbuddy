2684cd0cbef7592923e00ec51e17a8f9cd72e8c7
adwityac <adwityachakraborty@hotmail.com>
2025-11-24 19:10:23 +0530

    made some changes

--- client/src/data/mockItems.js
+++ client/src/data/mockItems.js
@@ -1,51 +1,44 @@
-import lawnmower from 'public\lawnmower.png'
-import drill from 'public\drillkit.png'
-import projector from 'public\projector.png'
-import keyboard from 'public\keyboard.png'
-import stroller from 'public\stroller.png'
-import camping from 'public\camping.png'
-
 const items = [
   {
     id: "1",
     title: "Lawn Mower",
     description: "Well maintained, available for borrowing.",
-    imageUrl: lawnmower,
+    imageUrl: "/lawnmower.png",
     availability: "available",
   },
   {
     id: "2",
     title: "Baby Stroller",
     description: "Well maintained, available for borrowing.",
-    imageUrl: stroller,
+    imageUrl: "/stroller.png",
     availability: "requested",
   },
   {
     id: "3",
     title: "Drill Kit",
     description: "Well maintained, available for borrowing.",
-    imageUrl: drill,
+    imageUrl: "/drillkit.png",
     availability: "available",
   },
   {
     id: "4",
     title: "Camping Tent",
     description: "Well maintained, available for borrowing.",
-    imageUrl: camping,
+    imageUrl: "/camping.png",
     availability: "requested",
   },
   {
     id: "5",
     title: "Keyboard",
     description: "Well maintained, available for borrowing.",
-    imageUrl: keyboard,
+    imageUrl: "/keyboard.png",
     availability: "available",
   },
   {
     id: "6",
     title: "Projector",
     description: "Well maintained, available for borrowing.",
-    imageUrl: projector,
+    imageUrl: "/projector.png",
     availability: "available",
   },
 ]
